25107e30ef62baf185712a555cbe6d4ea732a122
SaboorTunio <saboortunio2021@gmail.com>
2025-04-18 08:07:54 +0500

    Update todo.py

--- Todo-List-Manager/todo-cli-app/todo.py
+++ Todo-List-Manager/todo-cli-app/todo.py
@@ -4,4 +4,12 @@ import os # to check if the file exists
 
 TODO_FILE = "todo.json"
 
-def load_tasks
+def load_tasks ():
+    if os.path.exists(TODO_FILE):
+        return []
+    with open(TODO_FILE,"r") as file:
+        return json.load(file)
+
+def save_tasks(tasks):
+    with open(TODO_FILE,"w") as file:
+        json.dump(tasks,file, indent=4)    
